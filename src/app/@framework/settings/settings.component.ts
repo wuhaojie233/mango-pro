b362c5e99bac90bbffdd52b2a229040d10ca19cf
haha <33541346+wuhaojie233@users.noreply.github.com>
2020-10-30 19:35:17 +0800

    修改主题呀  fs

--- src/app/@framework/settings/settings.component.ts
+++ src/app/@framework/settings/settings.component.ts
@@ -2,11 +2,19 @@ import { Component, Input, OnInit } from '@angular/core'
 import { Settings } from '../../@shared/interface/settings'
 import { FrameworkService } from '../framework.service'
 
-// import less from 'less'
-// import LessPluginCleanCSS from 'less-plugin-clean-css'
-// import darkThemeVars from 'ng-zorro-antd/dark-theme'
-// import * as fs from 'fs'
+import less from 'less'
+// import * as LessPluginCleanCSS from 'less-plugin-clean-css'
+import darkThemeVars from 'ng-zorro-antd/dark-theme'
 // declare var fs: any
+declare var LessPluginCleanCSS: any
+import * as fs from 'fs'
+
+// 如果import后还是找不到fs，则用以下方法：
+
+// 定义：
+
+// 构造方法中加载：
+
 @Component({
   selector: 'mango-settings',
   templateUrl: './settings.component.html',
@@ -14,7 +22,6 @@ import { FrameworkService } from '../framework.service'
 })
 export class SettingsComponent implements OnInit {
   nzVisible = false
-
   get theme() {
     return this.frameService.navTheme
   }
@@ -36,14 +43,10 @@ export class SettingsComponent implements OnInit {
   }
 
   createheme() {
-    const less = require('less')
-    const LessPluginCleanCSS = require('less-plugin-clean-css')
-    const fs = require('fs')
-    const darkThemeVars = require('ng-zorro-antd/dark-theme')
-
-    const appStyles = 'path/src/styles.less' // 应用的样式入口文件
+    // const fs = require('fs')
+    // var fs = require('file-system')
+    const appStyles = 'assets/themes/default.less' // 应用的样式入口文件
     const themeContent = `@import '${appStyles}';`
-
     less
       .render(themeContent, {
         javascriptEnabled: true,
@@ -61,7 +64,7 @@ export class SettingsComponent implements OnInit {
       })
       .catch((e) => {
         // 记录渲染错误
-        console.error('记录渲染错误' + e)
+        console.error(e)
       })
   }
 
@@ -72,20 +75,20 @@ export class SettingsComponent implements OnInit {
   setThemeStyle(theme) {
     this.createheme()
     console.warn('theme---->' + theme)
-    if (theme === 'dark') {
-      const style = document.createElement('link')
-      style.type = 'text/css'
-      style.rel = 'stylesheet'
-      style.id = 'dark-theme'
-      style.href = 'assets/themes/style.dark.css'
-      // style.href = 'assets/themes/ng-zorro-antd.dark.min.css'
-      document.body.appendChild(style)
-    } else {
-      const dom = document.getElementById('dark-theme')
-      if (dom) {
-        dom.remove()
-      }
-    }
-    document.body.setAttribute('data-theme-style', theme)
+    // if (theme === 'dark') {
+    //   const style = document.createElement('link')
+    //   style.type = 'text/css'
+    //   style.rel = 'stylesheet'
+    //   style.id = 'dark-theme'
+    //   style.href = 'assets/themes/style.dark.css'
+    //   // style.href = 'assets/themes/ng-zorro-antd.dark.min.css'
+    //   document.body.appendChild(style)
+    // } else {
+    //   const dom = document.getElementById('dark-theme')
+    //   if (dom) {
+    //     dom.remove()
+    //   }
+    // }
+    // document.body.setAttribute('data-theme-style', theme)
   }
 }
