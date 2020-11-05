e089ec7bb96e49a281cc69483f7dcc244abbadba
haha <33541346+wuhaojie233@users.noreply.github.com>
2020-11-05 19:30:07 +0800

    主题更改完成(只差复制粘贴啦)
    啦啦啦啦啦啦

--- src/app/@framework/settings/settings.component.ts
+++ src/app/@framework/settings/settings.component.ts
@@ -10,7 +10,7 @@ import { FrameworkService } from '../framework.service'
 
 import less from 'less'
 // import * as LessPluginCleanCSS from 'less-plugin-clean-css'
-import darkThemeVars from 'ng-zorro-antd/dark-theme'
+// import darkThemeVars from 'ng-zorro-antd/dark-theme'
 declare var LessPluginCleanCSS: any
 
 @Component({
@@ -29,7 +29,13 @@ export class SettingsComponent implements OnInit {
     private frameService: FrameworkService,
     private zone: NgZone,
     private cdr: ChangeDetectorRef
-  ) {}
+  ) {
+    // const node = document.createElement('link')
+    // node.rel = 'stylesheet/less'
+    // node.type = 'text/css'
+    // node.href = 'assets/themes/default.less'
+    // document.getElementsByTagName('head')[0].appendChild(node)
+  }
 
   ngOnInit(): void {}
   setTheme(data) {
@@ -45,34 +51,11 @@ export class SettingsComponent implements OnInit {
     this.nzVisible = false
   }
 
-  createheme() {
-    // const fs = require('fs')
-    // var fs = require('file-system')
-    // const appStyles = 'assets/themes/default.less' // 应用的样式入口文件
-    // const themeContent = `@import '${appStyles}';`
-    const themeContent = require('ng-zorro-antd/ng-zorro-antd.less')
-    // const themeContent = `@import '${appStyles}';`
-    less
-      .render(themeContent, {
-        javascriptEnabled: true,
-        plugins: [new LessPluginCleanCSS({ advanced: true })],
-        modifyVars: {
-          // ...darkThemeVars,
-          '@primary-color': 'red',
-        },
-      })
-      .then((data) => {
-        this.globalCss = data.css
-        // fs.writeFileSync(
-        //   // 主题样式的输出文件
-        //   'path/assets/themes/style.dark.css',
-        //   data.css
-        // )
-      })
-      .catch((e) => {
-        // 记录渲染错误
-        console.error(e)
-      })
+  removeStyle() {
+    const dom = document.getElementById('dark-theme')
+    if (dom) {
+      dom.remove()
+    }
   }
 
   /**
@@ -80,35 +63,29 @@ export class SettingsComponent implements OnInit {
    * @param {*} data 颜色
    */
   setThemeStyle(theme) {
-    // this.createheme()
-    // return
-    console.warn('theme---->' + theme)
-    if (theme === 'dark') {
-      const node = document.createElement('link')
-      node.rel = 'stylesheet/less'
-      node.type = 'text/css'
-      node.href = 'assets/themes/default.less'
-      // document.getElementsByTagName('body')[0].appendChild(node)
-      // const style = document.createElement('link')
-      // style.type = 'text/css'
-      // style.rel = 'stylesheet'
-      // style.id = 'dark-theme'
-      // style.innerHTML = this.globalCss
-      // document.body.appendChild(node)
-      // document.getElementsByTagName('body')[0].appendChild(node)
-
-      less.modifyVars({ '@primary-color': 'blue' }).then(() => {
-        this.zone.run(() => this.cdr.detectChanges())
+    const appStyles = 'assets/themes/default.less' // 应用的样式入口文件
+    const themeContent = `@import '${appStyles}';`
+    less
+      .render(themeContent, {
+        javascriptEnabled: true,
+        plugins: [new LessPluginCleanCSS({ advanced: true })],
+        modifyVars: {
+          // ...darkThemeVars,
+          '@primary-color': theme,
+        },
       })
-      // setTimeout(()=>{
-
-      // },1000)
-    } else {
-      // const dom = document.getElementById('dark-theme')
-      // if (dom) {
-      //   dom.remove()
-      // }
-    }
-    // document.body.setAttribute('data-theme-style', theme)
+      .then((data) => {
+        this.globalCss = data.css
+        const style = document.createElement('style')
+        style.id = 'dark-theme'
+        style.innerHTML = this.globalCss
+        // document.body.appendChild(node)
+        this.removeStyle()
+        document.getElementsByTagName('head')[0].appendChild(style)
+      })
+      .catch((e) => {
+        // 记录渲染错误
+        console.error(e)
+      })
   }
 }
